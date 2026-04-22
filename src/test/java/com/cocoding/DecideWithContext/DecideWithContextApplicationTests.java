5625f7ef6e0e6a915db7a07dcad4303a52305e20
conor-clyde <conorclydesd@gmail.com>
2026-04-21 20:51:14 -0400

    initial Spring Boot project setup

---
+++ src/test/java/com/cocoding/DecideWithContext/DecideWithContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocoding.DecideWithContext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecideWithContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
